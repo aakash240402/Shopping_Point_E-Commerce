154ca1ee1b7dee7e76f877cfda91302b70345550
Aakash Mehta <96887522+aakash240402@users.noreply.github.com>
2023-08-07 01:41:20 +0530

    Add files via upload

---
+++ app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { SuggestedProduct } from '../models/models';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent{
+  
+}
